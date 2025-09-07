bf3149ee84a38463ed2d1ff89981a379144c4ae3
Shravani Keshav Patil <shravanipatil9160@gmail.com>
2025-09-07 12:40:09 +0530

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,31 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class LCABinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        // Search in left and right subtrees
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+    
+        if (left != null && right != null) {
+            return root;
+        }
+
+    
+        return left != null ? left : right;
+    }
+}
